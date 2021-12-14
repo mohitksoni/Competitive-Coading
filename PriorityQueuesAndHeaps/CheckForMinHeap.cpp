1344514338bf1b4ecdf74fcd28b046a36e823993
mohitksoni <95540516+mohitksoni@users.noreply.github.com>
2021-12-14 10:35:40 +0530

    Create CheckForMinHeap.cpp

---
+++ PriorityQueuesAndHeaps/CheckForMinHeap.cpp
@@ -0,0 +1,63 @@
+// Check for Min Heap
+
+// Given an array A of N integers, write an efficient program to check if A represents a min heap.
+// Note: Try solving without using in-built STL functions of heaps.
+
+// Convert Array to Min Heap
+
+// Given an array A of N integers, write a program to convert A into min-heap.
+// Note: Try solving without using in-built STL functions of heaps.
+// Do not sort the array; a sorted array output is not evaluated as the right answer.
+
+#include<algorithm>
+#include<iostream>
+#include<array>
+#define ll long long int
+using namespace std;
+
+int flag = 0;
+
+void min_heakify(ll a[], int i, int length){
+    int left = 2*i;
+    int right = 2*i + 1;
+    
+    int smallest;
+    if(left <= length && a[left] < a[i]){
+        smallest = left;
+    }
+    else{
+        smallest = i;
+    }
+    if(right <= length && a[right] < a[smallest]){
+        smallest = right;
+    }
+    if(smallest != i){
+        flag = 1;
+        swap(a[smallest], a[i]);
+        min_heakify(a, smallest, length);
+    }
+}
+
+void ConvertMinHeap(ll a[], int n){
+    for(int i = n/2;i>=1;i--){
+        min_heakify(a, i, n);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    
+    ll a[n + 1];
+    for(int i = 1;i<n + 1;i++){
+        cin>>a[i];
+    }
+    ConvertMinHeap(a,n);
+    
+    if(flag == 1){
+        cout<<"NO";
+    }
+    else{
+        cout<<"YES";
+    }
+}
